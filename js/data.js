e2631b0d1c9b1cbf83de62898cf7a4fb7d54a72d
Lguirelli <68300894+Lguirelli@users.noreply.github.com>
2026-05-30 20:45:37 -0300

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -8,17 +8,17 @@ window.OS_DATA = {
   ],
   portfolio: [
     { id:'design', label:'Design', type:'folder', path:'content/portfolio/design/index.html', files:[
-      { id:'landing-page-editavel', label:'landing page editavel', type:'project-folder', path:'content/portfolio/design/landing-page-editavel/index.html', projectPath:'content/portfolio/design/landing-page-editavel/index.html' },
-      { id:'interfaces', label:'interfaces', type:'folder-file', path:'content/portfolio/design/interfaces.html' },
-      { id:'identidade-visual', label:'identidade visual', type:'folder-file', path:'content/portfolio/design/identidade-visual.html' }
+      { id:'landing-page-editavel', label:'landing page editavel.html', type:'html-project', icon:'html', path:'content/portfolio/design/landing-page-editavel/index.html', projectPath:'content/portfolio/design/landing-page-editavel/index.html' },
+      { id:'interfaces', label:'interfaces.html', type:'html-file', icon:'html', path:'content/portfolio/design/interfaces.html' },
+      { id:'identidade-visual', label:'identidade visual.html', type:'html-file', icon:'html', path:'content/portfolio/design/identidade-visual.html' }
     ]},
     { id:'fotografia', label:'Fotografia', type:'folder', path:'content/portfolio/fotografia/index.html', files:[
-      { id:'editorial', label:'editorial', type:'folder-file', path:'content/portfolio/fotografia/editorial.html' },
-      { id:'tratamento-imagem', label:'tratamento de imagem', type:'folder-file', path:'content/portfolio/fotografia/tratamento-de-imagem.html' }
+      { id:'editorial', label:'editorial.html', type:'html-file', icon:'html', path:'content/portfolio/fotografia/editorial.html' },
+      { id:'tratamento-imagem', label:'tratamento de imagem.html', type:'html-file', icon:'html', path:'content/portfolio/fotografia/tratamento-de-imagem.html' }
     ]},
     { id:'videos', label:'Vídeos', type:'folder', path:'content/portfolio/videos/index.html', files:[
-      { id:'edicao', label:'edição', type:'folder-file', path:'content/portfolio/videos/edicao.html' },
-      { id:'motion', label:'motion', type:'folder-file', path:'content/portfolio/videos/motion.html' }
+      { id:'edicao', label:'edição.html', type:'html-file', icon:'html', path:'content/portfolio/videos/edicao.html' },
+      { id:'motion', label:'motion.html', type:'html-file', icon:'html', path:'content/portfolio/videos/motion.html' }
     ]}
   ],
   games: [
